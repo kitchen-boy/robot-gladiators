5d4b7eb13ff6b24e8b02c90d4ec79f284e4b9147
kitchen-boy <92004417+kitchen-boy@users.noreply.github.com>
2021-10-27 06:02:54 -0400

    Create & execute functions, interact with playe, store data

---
+++ assets/js/game.js
@@ -0,0 +1,22 @@
+var playerName = window.prompt("What is your robot's name?");
+
+// Note the lack of quotation marks around playerName window.alert(playerName);
+// What is this?
+console.log(playerName);
+console.log ("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10+10);
+//what is this?
+console.log("Our robot's anme is " + playerName)
+
+// "playerName is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight();
